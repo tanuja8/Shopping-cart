c414f7efc7228c9769f4f28c4dc491bd7596392c
Tanuja <tanujakhatun292@gmail.com>
2023-04-29 23:58:45 +0530

    project updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+
 import './App.css';
+import Header from './Component/Header';
 
 function App() {
-  return (
-    <div className='App'>
+  return <div className="App">
+    <Header></Header>
+  </div>
     
-    </div>
-  );
 }
 
 export default App;
--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Navbar,Container, FormControl } from 'react-bootstrap'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <Navbar bg='dark' variant='dark' style={{height:80}}>
+      <Container>
+        <Navbar.Brand>
+          <a href="/">Shopping Cart</a>
+        </Navbar.Brand>
+        <Navbar.Text className='search'>
+          <FormControl style={{width:500}} placeholder='Search a product' className='m-auto'/>
+          
+        </Navbar.Text>
+      </Container>
+    </Navbar>
   )
 }
 
