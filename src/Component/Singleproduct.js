c04dc98dde16e0d2778cb2ac06b277b8aad2315c
Tanuja <tanujakhatun292@gmail.com>
2023-06-14 02:18:16 +0530

    project updated

--- src/Component/Singleproduct.js
+++ src/Component/Singleproduct.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
 import Rating from './Rating';
+import { Button } from 'react-bootstrap';
 
 const Singleproduct = ({prod}) => {
   return (
@@ -9,7 +10,7 @@ const Singleproduct = ({prod}) => {
         <Card.Img variant='top' src={prod.image} alt={prod.name}/>
         <Card.Body>
           <Card.Title>{prod.name}</Card.Title>
-          <Card.Subtitle style={{paddingBottom:10}}>
+          <Card.Subtitle style={{paddingBottom: 10 }}>
             <span>₹ {prod.price.split(".")[0]}</span>
             {prod.fastDelivary ?(
               <div>Fast Delivary</div>
@@ -18,6 +19,13 @@ const Singleproduct = ({prod}) => {
             )}
             <Rating rating={prod.ratings}></Rating>
           </Card.Subtitle>
+          <Button variant='danger'>
+              Remove from cart
+            </Button>
+            <Button disabled={!prod.inStock}>
+              {!prod.inStock ? "out of stock" : "Add to cart"}
+               
+            </Button>
         </Card.Body>
       </Card>
     </div>
