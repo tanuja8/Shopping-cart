9ec7bf6f7ad4959015cd01b8f76cc6ee348049ec
Tanuja <tanujakhatun292@gmail.com>
2023-06-13 22:37:02 +0530

    create card product area

--- src/Component/Singleproduct.js
+++ src/Component/Singleproduct.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
+import Rating from './Rating';
 
 const Singleproduct = ({prod}) => {
   return (
-    <div>
-      {prod.name}
+    <div className='products'>
+      <Card>
+        <Card.Img variant='top' src={prod.image} alt={prod.name}/>
+        <Card.Body>
+          <Card.Title>{prod.name}</Card.Title>
+          <Card.Subtitle style={{paddingBottom:10}}>
+            <span>₹ {prod.price.split(".")[0]}</span>
+            {prod.fastDelivary ?(
+              <div>Fast Delivary</div>
+            ):(
+              <div>4 days Delivary</div>
+            )}
+            <Rating rating={prod.ratings}></Rating>
+          </Card.Subtitle>
+        </Card.Body>
+      </Card>
     </div>
   )
 }
