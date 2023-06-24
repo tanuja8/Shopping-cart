a3c1e2d97e01413df040170550f5a93486503faf
Tanuja <tanujakhatun292@gmail.com>
2023-06-24 23:53:21 +0530

    creat e reducer for product price details

--- src/Component/Cart.js
+++ src/Component/Cart.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { CartState } from './Context/Context'
-import { ListGroup } from 'react-bootstrap';
+import { Button, ListGroup } from 'react-bootstrap';
 
 const Cart = () => {
   const{
     state:{cart},
     dispatch,
   } =CartState();
+
+  const [total,setTotal]=useState();
+  useEffect(()=>{
+    setTotal(cart.reduce((acc,curr)=> acc+Number(curr.price),0));
+  },[cart]);
   return (
     <div className='home'>
       <div className='productcontainer'>
@@ -20,6 +25,10 @@ const Cart = () => {
       </div>
       <div className='filter summary'>
         <span className='title'>Subtotal({cart.length}) items</span>
+        <span style={{fontWeight:700,fontSize:20}}>Total: ₹ {total}</span>
+        <Button type='button' disabled={cart.length==0}>
+          Proceed to Checkout
+        </Button>
       </div>
     </div>
   )
