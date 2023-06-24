979aab15db74f0d2ecac969d96b42d7d05f9cc29
Tanuja <tanujakhatun292@gmail.com>
2023-06-24 23:33:52 +0530

    project updated

--- src/Component/Cart.js
+++ src/Component/Cart.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { CartState } from './Context/Context'
+import { ListGroup } from 'react-bootstrap';
 
-function Cart() {
+const Cart = () => {
+  const{
+    state:{cart},
+    dispatch,
+  } =CartState();
   return (
-    <div>
-
+    <div className='home'>
+      <div className='productcontainer'>
+      <ListGroup>
+        {
+          cart.map(prod=>(
+            <span>{prod.name}</span>
+          ))
+        }
+      </ListGroup>
+      </div>
+      <div className='filter summary'>
+        <span className='title'>Subtotal({cart.length}) items</span>
+      </div>
     </div>
   )
 }
