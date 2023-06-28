daab543e4b962bc9132c4ebe3dc3cd54c36979bb
Tanuja <tanujakhatun292@gmail.com>
2023-06-28 23:26:37 +0530

    project updated

--- src/Component/Cart.js
+++ src/Component/Cart.js
@@ -64,7 +64,7 @@ const Cart = () => {
       <div className='filter summary'>
         <span className='title'>Subtotal({cart.length}) items</span>
         <span style={{fontWeight:700,fontSize:20}}>Total: ₹ {total}</span>
-        <Button type='button' disabled={cart.length==0}>
+        <Button type='button'  disabled={cart.length==0}>
           Proceed to Checkout
         </Button>
       </div>
